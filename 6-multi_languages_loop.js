afee58bd3ca02ab4c863cf484f4d9be2cdb10227
kephasitati <kephasitati@gmail.com>
2025-07-12 22:50:05 +0300

    Multi Loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+const result = [];
+
+for (let i = 0; i < lines.length; i++) {
+  result.push(lines[i]);
+}
+
+console.log(result.join('\n'));
